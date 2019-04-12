a2d27b830bf155bd644aab7cbbd174cfe0c3e23a
Andrey Sakharov <sakharov.a@ittest-team.ru>
2019-04-12 21:41:52 +0300

    using jsx extantions

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Routes from './components/Routes';
+import Routes from './components/Routes.jsx';
 import TopNavigation from './components/topNavigation';
 import SideNavigation from './components/sideNavigation.jsx';
 import Footer from './components/Footer';
--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
