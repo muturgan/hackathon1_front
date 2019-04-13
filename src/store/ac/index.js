3e82a0a07ebb4474d4097f2c1c31a05e98519b69
Andrey Sakharov <sakharov.a@ittest-team.ru>
2019-04-13 16:35:34 +0300

    using action creator for login

--- src/store/ac/index.js
+++ src/store/ac/index.js
@@ -1,7 +1,16 @@
 import { USER_UPDATE_DATA, USER_LOGOUT } from '../constants'
 
-export function userGetData() {
-    return { type: USER_UPDATE_DATA };
+export function userLogin(data) {
+    return {
+        type: USER_UPDATE_DATA,
+        payload: {
+            name: data.userData.real_name,
+            sex: data.userData.sex,
+            email: data.userData.default_email,
+            token: data.jwtToken,
+            avatar: data.userData.is_avatar_empty ? null : `https://avatars.yandex.net/get-yapic/${data.userData.default_avatar_id}/islands-200`,
+        },
+    };
 };
 
 export function userLogout() {
