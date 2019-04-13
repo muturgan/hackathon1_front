5b4ff616d190a02295dfb6050b56c6479cf7583e
Andrey Sakharov <sakharov.a@ittest-team.ru>
2019-04-13 23:01:50 +0300

    clean pagination

--- src/components/my/Pagination.jsx
+++ src/components/my/Pagination.jsx
@@ -13,45 +13,25 @@ export const Pagination = (props) => {
       <MDBCol>
         <MDBPagination className="mb-5">
 
-          {+currentPage === 1
-            ? <MDBPageItem disabled>
-                <MDBPageNav aria-label="Previous">
-                  <span aria-hidden="true">&laquo;</span>
-                </MDBPageNav>
-              </MDBPageItem>
-            : <MDBPageItem>
-              <MDBPageNav aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-              </MDBPageNav>
-            </MDBPageItem>
-          }
+          <MDBPageItem disabled={+currentPage === 1}>
+            <MDBPageNav aria-label="Previous">
+              <span aria-hidden="true">&laquo;</span>
+            </MDBPageNav>
+          </MDBPageItem>
 
           {pages.map(number => (
-            number === +currentPage
-              ? <MDBPageItem active>
-                  <MDBPageNav>
-                    {number}
-                  </MDBPageNav>
-                </MDBPageItem>
-              : <MDBPageItem>
-                  <MDBPageNav>
-                    {number}
-                  </MDBPageNav>
-                </MDBPageItem>
-          ))}
-
-          {+currentPage === +pagesCount
-            ? <MDBPageItem disabled>
-                <MDBPageNav aria-label="Previous">
-                  <span aria-hidden="true">&raquo;</span>
-                </MDBPageNav>
-              </MDBPageItem>
-            : <MDBPageItem>
-              <MDBPageNav aria-label="Previous">
-                <span aria-hidden="true">&raquo;</span>
+            <MDBPageItem active={number === +currentPage}>
+              <MDBPageNav>
+                {number}
               </MDBPageNav>
             </MDBPageItem>
-          }
+          ))}
+
+          <MDBPageItem disabled={+currentPage === +pagesCount}>
+            <MDBPageNav aria-label="Previous">
+              <span aria-hidden="true">&raquo;</span>
+            </MDBPageNav>
+          </MDBPageItem>
 
         </MDBPagination>
       </MDBCol>
