dbce9a45585b8c34008701e91322e1ef4583a1c3
Andrey Sakharov <sakharov.a@ittest-team.ru>
2019-04-15 18:03:16 +0300

    replace src with href in anchor tags

--- src/components/pages/InfoPage.jsx
+++ src/components/pages/InfoPage.jsx
@@ -20,25 +20,25 @@ export default class InfoPage extends React.Component {
               }}
             >
               <ul style={{textAlign: 'left'}}>
-                <li><a src="https://github.com/muturgan/hackathon1_back" target="_blank">
+                <li><a href="https://github.com/muturgan/hackathon1_back" target="_blank">
                   Ссылка на репозиторий с серверной частью приложения                
                 </a></li>
-                <li><a src="https://github.com/muturgan/hackathon1_front" target="_blank">
+                <li><a href="https://github.com/muturgan/hackathon1_front" target="_blank">
                   Ссылка на репозиторий с клиентской частью приложения                
                 </a></li>
                 <hr/>
-                <li><a src="https://mdbootstrap.com/docs/react/" target="_blank">
+                <li><a href="https://mdbootstrap.com/docs/react/" target="_blank">
                   Ссылка на документацию к использованной библиотеке графических компонентов
                 </a></li>
-                <li><a src="https://benhowell.github.io/react-grid-gallery/" target="_blank">
+                <li><a href="https://benhowell.github.io/react-grid-gallery/" target="_blank">
                   Ссылка на документацию к использованной библиотеке галереи (слегка модифицировал ее для реализации функционала лайков)
                 </a></li>
                 <hr/>
-                <li><a src="https://pixabay.com/ru/" target="_blank">
+                <li><a href="https://pixabay.com/ru/" target="_blank">
                   Все картинки (хоть они сейчас и лежат на яндекске) былипозаимствованы с сайта pixabay
                 </a></li>
                 <hr/>
-                <li><a src="https://oauth.yandex.ru/client/2047cbd294a943cf80d51ae0227cb844" target="_blank">
+                <li><a href="https://oauth.yandex.ru/client/2047cbd294a943cf80d51ae0227cb844" target="_blank">
                   Ссылка на зарегистрированное яндекс приложение
                 </a></li>
               </ul>
