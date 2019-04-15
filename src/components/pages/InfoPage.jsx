91b5ba429ad935834f29829ae4b89886c2aad718
Andrey Sakharov <sakharov.a@ittest-team.ru>
2019-04-15 15:45:27 +0300

    info page content and typesetting

--- src/components/pages/InfoPage.jsx
+++ src/components/pages/InfoPage.jsx
@@ -10,11 +10,38 @@ export default class InfoPage extends React.Component {
     return (
       <MDBRow>
         <MDBCol md="12">
-          <MDBCard className="mt-5">
+          <MDBCard>
             <MDBCardBody
               className = "text-center"
-              style={{ width: '100%'}}
+              style={{
+                width: '100%',
+                minHeight: `calc(${document.documentElement.clientHeight}px - 139px - 6rem)`,
+                color: 'darkblue',
+              }}
             >
+              <ul style={{textAlign: 'left'}}>
+                <li><a src="https://github.com/muturgan/hackathon1_back" target="_blank">
+                  Ссылка на репозиторий с серверной частью приложения                
+                </a></li>
+                <li><a src="https://github.com/muturgan/hackathon1_front" target="_blank">
+                  Ссылка на репозиторий с клиентской частью приложения                
+                </a></li>
+                <hr/>
+                <li><a src="https://mdbootstrap.com/docs/react/" target="_blank">
+                  Ссылка на документацию к использованной библиотеке графических компонентов
+                </a></li>
+                <li><a src="https://benhowell.github.io/react-grid-gallery/" target="_blank">
+                  Ссылка на документацию к использованной библиотеке галереи (слегка модифицировал ее для реализации функционала лайков)
+                </a></li>
+                <hr/>
+                <li><a src="https://pixabay.com/ru/" target="_blank">
+                  Все картинки (хоть они сейчас и лежат на яндекске) былипозаимствованы с сайта pixabay
+                </a></li>
+                <hr/>
+                <li><a src="https://oauth.yandex.ru/client/2047cbd294a943cf80d51ae0227cb844" target="_blank">
+                  Ссылка на зарегистрированное яндекс приложение
+                </a></li>
+              </ul>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
